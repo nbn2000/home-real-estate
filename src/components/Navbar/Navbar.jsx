82cb76994279b39b84df887238c666b29a21276b
nbn2000 <makhmudov4744@list.ru>
2024-03-05 12:14:18 +0500

    navbar started

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,10 @@ const Navbar = () => {
     <div className="w-full h-[134px] px-[7.5rem] flex flex-row justify-between items-center">
       <img src={Logo} alt="logo" />
       <div className="relative">
-        <button className="bg-blue" onClick={() => setOpen(true)}>
+        <button
+          className="bg-green-200 w-max h-max p-10"
+          onClick={() => setOpen(true)}
+        >
           <Menu />
         </button>
         <MenuModal open={open} onClose={() => setOpen(false)} />
